99cf3d8c259da701e9e1808bbdabe6e1cf8de2a8
Jong Woong To <jongwoongto@localhost.localdomain>
2019-04-26 15:50:08 +0900

    26/04/19 django forms

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,23 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home_page(request):
-    context = {
-        "title":"home page"
-    } 
+    context = {"title": "home page"}
     return render(request, "home_page.html", context)
+
 
 def about_page(request):
-    context = {
-        "title" : "about page"
-    }
+    context = {"title": "about page"}
     return render(request, "home_page.html", context)
 
+
 def contact_page(request):
-    context = {
-        "title" : "contact page"
-    }
-    return render(request, "home_page.html", context)
+    context = {"title": "contact page"}
+    if request.method == "POST":
+        print(request.POST.get("fullname"))
+    return render(request, "form.html", context)
+
 
 def home_page_old(request):
     # When you go to a url this function is called
