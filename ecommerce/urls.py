a02dda05bc2fb943af1717a7ee5617b7a9dda0e2
JongW <jwto13@gmail.com>
2019-04-26 12:46:42 +0900

    26/04/19 lunch time work - learning about context in views

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path
+
 # Import the home_page function that we created
-from .views import home_page
+from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     # When the url path is blank call the function home_page  
     path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls)
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render
 
-
 def home_page(request):
+    context = {
+        "title":"home page"
+    } 
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title" : "about page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title" : "contact page"
+    }
+    return render(request, "home_page.html", context)
+
+def home_page_old(request):
     # When you go to a url this function is called
     # Takes request as an argument and returns the Http Response
-    return HttpResponse("Hello World")
+    return HttpResponse("<h1>Hello World<h1>")
